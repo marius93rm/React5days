d22ee97edafe04a1b69e42e72292ee7df78c293a
marius93rm <marius93rm@gmail.com>
2022-09-06 09:04:20 +0200

    Start day 2

---
+++ Giorno1/eserc2.js
@@ -0,0 +1,11 @@
+//Stampare una matrice quadrata con un carattere lungo le diagonali e tutti 0 altrove.
+// grandezza e carattere a scelta
+
+
+//scelgo la grandezza
+//scelgo il carattere
+//creo l'array di grandezza
+//per ogni elemento dell'array ci metto dentro un array di grandezza pieno di 0
+
+//scorro tutto l'array
+//se mi trovo in una posizione che sta sulla diagonale ci metto uno 0
