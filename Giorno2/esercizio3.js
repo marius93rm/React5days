759fb83b4b6372050a40b19d70af51d1004f3097
marius93rm <marius93rm@gmail.com>
2022-09-06 14:03:56 +0200

    2 parte

--- Giorno2/esercizio3.js
+++ Giorno2/esercizio3.js
@@ -1,14 +1,10 @@
 const lib = require("./eserc2"); //importo stampaCarina
 
-// Riempire una tabella di 4 righe e 3 colonne con
-// interi pseudo-casuali compresi tra 1 e 9 (estremi
-// inclusi). Per ciascuna riga della tabella
-// visualizzare la somma degli interi presenti sulla
-// riga e visualizzare la somma degli interi su
-// ciascuna colonna
+// Riempire una tabella di 4 righe e 3 colonne con interi pseudo-casuali compresi tra 1 e 9 (estremi inclusi).
+// Per ciascuna riga della tabella visualizzare la somma degli interi presenti sulla riga e visualizzare la somma degli interi su ciascuna colonna
 
-const rig = 4
-const col = 3
+const rig = 9
+const col = 9
 
 const mioArra = Array(rig) //creo le righe
 
@@ -17,10 +13,36 @@ for (let i=0; i < rig; i++){ //scorro tutte le righe
     for (let j = 0; j < col; j++){ //scorro gli elementi della riga i
         mioArra[i][j]= Math.floor(Math.random() * (9 - 1)) + 1 //creo un valore random e lo assegno all'elemento di indici i,j
     }
-    console.log(mioArra[i].join(" "))
+    //console.log(mioArra[i].join(" "))
 }
+const risultatoRighe = Array(rig).fill(0)
+const risultatoColonne = Array(col).fill(0)
+
 
 //la scorro con l'obiettivo di:
+for (let i=0; i<rig; i++){
+    for (let j=0; j <col; j++){
+        risultatoRighe[i] += mioArra[i][j]
+        risultatoColonne[j] += mioArra[i][j]
+    }
+}
+
+// for (let i = 0; i <mioArra.length; i++){
+//     let stampa = ""
+//     for (let j = 0; j < mioArra[i].length; j++) {
+//         stampa+= mioArra[i][j]+" "
+//     }
+//     stampa+=risultatoRighe[i]
+//     console.log(stampa)
+// }
+
+for (let i = 0; i <mioArra.length; i++){
+    console.log( mioArra[i].join(" ") + " |" + risultatoRighe[i])
+}
+console.log("________")
+console.log(risultatoColonne.join(" "))
+
+
 //calcolare la somma di OGNI riga
 //calcolare la somma di OGNI colonna
 
---
+++ Giorno2/refresher2.js
@@ -0,0 +1,96 @@
+// const numeretto = "3"
+
+// if (numeretto === 3){ //comparazione senza tenere conto del tipo
+//     console.log("true")
+// }
+// else {
+//     console.log("false")
+// }
+
+// if (numeretto > 3){
+//     console.log("maggiore")
+// }
+// else if(numeretto == 3){
+//     console.log("uguale")
+// }
+// else{
+//     console.log("minore")
+// }
+
+// switch (numeretto){
+//     case 1:
+//         console.log("contiene 1")
+//         break
+//     case 2:
+//         console.log("contiene 2")
+//         break
+//     default:
+//         console.log("non è nessuno di questi")
+// }
+
+// //operatore ternario
+// let staPiovendo = true
+// staPiovendo ? console.log("sta piovendo") : console.log("non sta piovendo")
+// //condizione ? true : false
+
+// //while
+// let valore = 3
+// while (valore > 0){
+//     console.log(count)
+//     count--
+// }
+
+const vettore = Array(10).fill(10)
+//FOR ciclo definito
+for(let i=0;i<vettore.length;i++){
+    vettore[i]-=i
+}
+
+//WHILE ciclo indefinito
+// let indice = 100
+// while (indice<vettore.length){
+//     vettore[indice]-=indice
+//     indice++
+// }
+
+//DO WHILE almeno un giro lo faccio
+// let indice = 0
+// do {
+//     vettore[indice]-=indice
+//     indice++
+// } while(indice<vettore.length)
+// console.log(vettore)
+
+//FOR OF per gli array
+// for (const numero of vettore){
+//     console.log(numero/2)
+// }
+
+// //FOR EACH sugli array
+// vettore.forEach( (number, i, arr) => {
+//     console.log(number, i)
+// } )
+
+//FOR IN sugli oggetti
+// const user = {
+//     nome: "Marius",
+//     cognome: "Minia"
+// }
+// console.log(  user["nome"]  )
+
+// for(const key in user){
+//     console.log(key, user[key])
+// }
+
+
+//Object
+const person = {}
+const rettangolo = {
+    length: 29,
+    width: 20
+}
+console.log(rettangolo)
+
+for (const chiave in rettangolo){
+    console.log("la "+chiave+" è: "+rettangolo[chiave]+" ")
+}
