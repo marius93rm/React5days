09321b50367936ee62a8144143c7b6ba35b177f2
marius93rm <marius93rm@gmail.com>
2022-09-07 17:41:18 +0200

    App completa e carina :)

--- Giorno3bis/terza-app/src/index.js
+++ Giorno3bis/terza-app/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './primitive.css';
-import profilo from './profilo.jpg'
+
+//imgs
+import profilo from './imgs/profilo.jpg'
+import spunta from './imgs/spunta.png'
+import html from './imgs/html.png'
+import css from './imgs/css.png'
+import js from './imgs/js.png'
+import reac from './imgs/react.png'
+
 
 const oggi = new Date().toLocaleDateString()
 const autore = {
@@ -9,61 +17,147 @@ const autore = {
   cognome: "Minia"
 }
 
-//STILE
+//HEADER
 const headerStlyes = {
-  backgroundColor: "#61DBFB",
+  backgroundColor: "#F5E2C8",
   fontFamily: "Helvetica Neue",
   padding: 25,
   lineHeight: 1.5
 }
 
+const header = (
+  <header style={headerStlyes}>
+    <h1 className="centro">Benvenuti nel nostro sito</h1>
+    <h2 style={{ margin: "10px 0 0 0" }}>Cominciamo con il tutorial di React</h2>
+    <h3 style={{ margin: "0px" }}>una libreria per creare pezzi di UI</h3>
+    <small>{oggi}</small>
+  </header>
+)
+
+//UTENTE
 const utenteStyles = {
-  display: "inline-block",
+  marginTop: "30px",
   width: "150px",
   height: "150px",
   borderRadius: "50%",
   objectFit: "cover"
 }
 
-const mainStyles = {
-  backgroundColor: "#F3F0F5"
+const spuntaStyles = {
+  width: "auto",
+  height: "15px",
+  marginLeft: "5px"
 }
 
-const footerStyles = {
-  backgroundColor: "#61DBFB"
+const skillsStyles = {
+  display: "inline-block",
+  color:"white",
+  background:"#47D0CF",
+  maxWidth: "100px",
+  padding: "5px 10px 5px 10px",
+  textAlign: "center",
+  borderRadius: "10px",
+  margin: "10px",
 }
 
-//ELEMENTI
-const header = (
-  <header style={headerStlyes} className="small-container">
-    <h1>Benvenuti nel nostro sito</h1>
-    <h2>Cominciamo con il tutorial di React</h2>
-    <h3>una libreria per creare pezzi di UI</h3>
-    <small>{oggi}</small>
-  </header>
+const skills = ["HTML", "CSS", "Javascript", "Python", "Java", "Scala", "NodeJS", "SQL", "React", "Angular", "Git", "MondoFB", "Flask", "Pandas", "NumPy", "MySQL"]
+const skillsFormatted = skills.map((elem, index) =>
+  <div key={index} style={skillsStyles}>
+    {elem}
+  </div>
 )
 
 const utente = (
   <div>
-    <img src={profilo} alt="Napoleone" style={utenteStyles}/>
-    <p>{autore.nome} {autore.cognome.toUpperCase()}</p>
+    <div className="centro">
+      <img src={profilo} alt="Napoleone" style={utenteStyles} />
+      <p style={{ margin: "0" }}>
+        {autore.nome.toUpperCase()} {autore.cognome.toUpperCase()}
+        <img style={spuntaStyles} src={spunta} alt="utente verificato" />
+      </p>
+      <small>Teacher, Rome</small>
+    </div>
+    <h5 style={{ textAlign: "left" }}>SKILLS</h5>
+    {skillsFormatted}
   </div>
 )
 
-const techs = ["HTML","CSS","Javascript"]
-const techsFormatted = techs.map( (tech, index) => <li key={index}> {tech}</li>)
+//TECHs
+const techStyles = {
+  width: "auto",
+  height: "150px",
+  marginBottom: "10px",
+  padding: "0px 10px 0px 10px"
+}
+
+const techs = [html, css, js, reac]
+const techsFormatted = techs.map((tech, index) =>
+  <div className="flex-small" key={index}>
+    <img alt="tecnologia" src={tech} style={techStyles} />
+  </div>)
+
+//MAIN
+const mainStyles = {
+  backgroundColor: "#F5E2C8"
+}
 
 const main = (
-  <main style={mainStyles} className="small-container">
-    <p>Prerequisiti per iniziare con react.js</p>
-    <ul>
+  <main style={mainStyles}>
+    <h3 style={{ padding: "25px 0px 25px 0" }} className="centro">Front End Technologies</h3>
+    <div className="flex-row">
       {techsFormatted}
-    </ul>
+    </div>
+
   </main>
 )
 
+//SUBSCRIBE
+const subscribeStyles = {
+  backgroundColor: "#C2E6F4"
+}
+
+const inputStyles = {
+  display: "flex"
+}
+
+const textStyles = {
+  marginLeft: "10px",
+  marginRight: "10px",
+  backgroundColor: "white"
+}
+
+const buttonStyles = {
+  marginTop: "10px",
+  backgroundColor: "#F37475",
+  border: "0px",
+  borderRadius: "10px",
+  width: "300px"
+}
+
+const subscribe = (
+  <div className="centro" style={subscribeStyles}>
+    <h3 className="centro">Subscribe</h3>
+    <p>Sign up with your email address to receive news and updates.</p>
+    <div style={inputStyles}>
+      <input style={textStyles} type="text" name="firstName" placeholder='First name' />
+      <input style={textStyles} type="text" name="lastName" placeholder='Last name' />
+      <input style={textStyles} type="text" name="email" placeholder='Email' />
+    </div>
+
+    <input style={buttonStyles} type="submit" value="Subscribe" />
+
+  </div>
+)
+
+
+//FOOTER
+const footerStyles = {
+  backgroundColor: "#17255A",
+  color: "white"
+}
+
 const footer = (
-  <footer styles={footerStyles} className="small-container" >
+  <footer style={footerStyles}>
     <p>Copyright 2022</p>
   </footer>
 )
@@ -71,10 +165,11 @@ const footer = (
 
 //RENDERIZZA
 const app = (
-  <div>
+  <div className="small-container">
     {header}
     {utente}
     {main}
+    {subscribe}
     {footer}
   </div>
 )
